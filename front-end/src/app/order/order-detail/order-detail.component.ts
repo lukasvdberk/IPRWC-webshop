6324243976eef03e09b0f97f2498a042fd211862
lukasberk <lukasvdberk@gmail.com>
2020-12-24 13:58:17 +0100

    [Fixed] Detail page of orders when you are an admin you have access to all order detail pages.

--- front-end/src/app/order/order-detail/order-detail.component.ts
+++ front-end/src/app/order/order-detail/order-detail.component.ts
@@ -13,6 +13,7 @@ import {AuthenticationService} from "../../authentication/authentication.service
   styleUrls: ['./order-detail.component.css']
 })
 export class OrderDetailComponent implements OnInit {
+  orderId = -1
   order: Order | undefined
   isAdmin: boolean | undefined = false
 
@@ -21,26 +22,40 @@ export class OrderDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.isAdmin = this.authService.isAdmin()
+
     this.route.params.subscribe(params => {
-       this.customerService.getCustomer().then((customer) => {
-         if (customer) {
-           this.orderService.getOrdersOfCustomer(customer).subscribe((orders) => {
-            const existingOrderIndex = orders.findIndex((order) => order.id == params['orderId'])
-            if (existingOrderIndex !== -1) {
-              this.order = orders[existingOrderIndex];
-              console.log(this.order.productOrders)
-            } else {
-              // TODO redirect to 404
-            }
-           }, error => {
-             this.toastService.showError({
-               message: 'Failed to fetch your orders-from-customer please try again later',
-               durationInSeconds: 3
-             })
-           })
-         }
-       })
+       this.orderId = +params['orderId']
+
+       // If we are an admin we have access to all orders and want to filter through that
+       if(this.isAdmin) {
+         this.orderService.getAllOrders().subscribe(this.successOrderResponse.bind(this), this.errorSuccessOrderResponse.bind(this))
+       } else {
+         this.customerService.getCustomer().then((customer) => {
+           if (customer) {
+             this.orderService.getOrdersOfCustomer(customer).subscribe(this.successOrderResponse.bind(this), this.errorSuccessOrderResponse.bind(this))
+           }
+         })
+       }
     });
+  }
+
+  successOrderResponse(orders: Order[]): void {
+    const existingOrderIndex = orders.findIndex((order) => {
+      return order.id == this.orderId
+    })
+    if (existingOrderIndex !== -1) {
+      this.order = orders[existingOrderIndex];
+      console.log(this.order.productOrders)
+    } else {
+      // TODO redirect to 404
+    }
+  }
+
+  errorSuccessOrderResponse(error: any): void {
+    this.toastService.showError({
+      message: 'Failed to fetch your orders-from-customer please try again later',
+      durationInSeconds: 3
+    })
   }
 
   calculateTotal(): number {
